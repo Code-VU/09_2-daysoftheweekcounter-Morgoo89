564adf2e87687f18666b240376a098aa6544441f
Morgan Williams <morgwilliams7@gmail.com>
2025-03-23 18:19:27 -0500

    day_counts

--- counter.py
+++ counter.py
@@ -1,6 +1,26 @@
 def countDayOfTheWeek():
     # This first line is provided for you
+    day_counts = dict()
     file_name = input("Enter a file name: ")
+    try:
+        file_hand = open(file_name)
+    except:
+        print('File cannot be opened:', file_name)
+        exit()
+        
+    for line in file_hand:
+        words = line.split()
+        if len(words) < 3 or words[0] != 'From':
+            continue
+        else:
+            if words[2] not in day_counts:
+                day_counts[words[2]] = 1    #First entry
+            else:
+                day_counts[words[2]] += 1   #Additional entries
+    print(day_counts)  
+    
+   
+    
 
 
 
